ba374e6feed3e091502ea6ee8b98c9362c45d88e
Francisco Igor <franciscoigor@gmail.com>
2018-04-19 21:13:42 -0700

    Added doc and fixes

--- src/db/Generator.java
+++ src/db/Generator.java
@@ -2,51 +2,107 @@ package db;
 
 import java.util.Random;
 
+/**
+ * Data generator class
+ * @author franciscoigor@gmail.com
+ *
+ */
 public class Generator {
 	
+	/**
+	 * Random generator
+	 */
 	private static Random rnd;
 	
 	static {
 		rnd=new Random();
 	}
 	
+	/**
+	 * To fix or set a random seed
+	 * @param seed
+	 */
 	public static void setSeed(long seed){
 		rnd.setSeed(seed);
 	}
 	
+	/**
+	 * Get a random number between range
+	 * @param min Minimum number
+	 * @param max Maximum number (inclusive)
+	 * @return Random number
+	 */
 	public static int getNumber(int min,int max){
 		int number=rnd.nextInt(1+max-min)+min;
 		return number;
 	}
 	
+	/**
+	 * Returns a random digit (0-9) 
+	 * @return Random digit
+	 */
 	public static int getDigit(){
 		return getNumber(0,9);
 	}
 		
+	/**
+	 * Return a letter from a list of letters
+	 * @param letters Letters to pick a random letter
+	 * @return A single character from the source string
+	 */
 	public static char getChar(String letters){
 		return letters.charAt(getNumber(0, letters.length()-1));
 	}
 	
+	/**
+	 * Returns a vowel letter (lowercase)
+	 * @return Vowel letter
+	 */
 	public static char getVowel(){
 		return getChar("aeiou");
 	}
 	
+	/**
+	 * Returns a vowel letter (uppercase)
+	 * @return Vowel letter
+	 */
 	public static char getUpperVowel(){
 		return getChar("AEIOU");
 	}
 	
+	/**
+	 * Returns a consonant letter (lowercase)
+	 * @return Consonant letter
+	 */
 	public static char getConsonant(){
 		return getChar("bcdfghjklmnpqrstvwxyz");
 	}
 	
+	/**
+	 * Returns a consonant letter (uppercase)
+	 * @return Consonant letter
+	 */
 	public static char getUpperConsonant(){
 		return getChar("BCDFGHJKLMNPQRSTVWXYZ");
 	}
 	
+	/**
+	 * Get a random word with a fixed number of letters
+	 * @param letters Number of letters
+	 * @param upperCaseFirst True to enable upper-case the first letter 
+	 * @return A random word
+	 */
 	public static String getWord(int letters, boolean upperCaseFirst){
 		return getWord(letters, letters, upperCaseFirst);
 	}
 	
+	/**
+	 * Get a random word with a variable number of letters (range)
+	 * @param minLetters Minimum number of letters
+	 * @param maxLetters Maximum number of letters
+	 * @param upperCaseFirst True to enable upper-case the first letter 
+	 * @return A random word
+	 */
 	public static String getWord(int minLetters, int maxLetters, boolean upperCaseFirst){
 		int letters=getNumber(minLetters, maxLetters);
 		StringBuffer buffer=new StringBuffer();
@@ -65,7 +121,12 @@ public class Generator {
 		}
 		return buffer.toString();
 	}
-	
+
+	/**
+	 * Returns a sentence with a number of random words
+	 * @param words Number of words
+	 * @return A sentence with random words
+	 */
 	public static String getSentence(int words){
 		StringBuffer buffer=new StringBuffer();
 		buffer.append(getWord(getNumber(5, 7),true));
@@ -75,14 +136,5 @@ public class Generator {
 		}
 		return buffer.toString();
 	}
-		
-	
-	
-	public static void main(String[] args) {
-		for (int i = 0; i < 30; i++) {
-			System.out.println(getSentence(5));			
-		}
-	}
-	
-	
+
 }
